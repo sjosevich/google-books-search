9e761242d66687d1ed8b97a26fd45dfd6be80929
Serguei Rodriguez <sjosevich@gmail.com>
2020-03-07 22:57:26 -0700

    Added my routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import MyNavBar from './components/MyNavBar'
 import MyHeader from './components/MyHeader';
+import Wrapper from "./components/Wrapper";
 
 class App extends Component {
  
@@ -12,6 +13,12 @@ class App extends Component {
         <div>
           <MyNavBar />
           <MyHeader />
+          <Wrapper>
+          <Route exact path="/"  />
+          <Route exact path="/search"  />
+          <Route exact path="/saved"  />
+          <Route exact path="/noMatch"  />
+        </Wrapper>
         </div>
       </Router>
     )
