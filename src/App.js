3f229245a4893200fcdaea3b0747c656e7245909
Serguei Rodriguez <sjosevich@gmail.com>
2020-03-07 22:38:28 -0700

    Adding my nav bar component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,18 @@
-import React, {Component} from 'react';
-import {Table, Button} from 'reactstrap';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import MyNavBar from './components/MyNavBar'
 
 class App extends Component {
  
   render() {
     return (
-      
-        <div className="App" container>
-            <Table>
-              <thead>
-                <tr>
-                  <th>authors</th>
-                  <th>description</th>
-                  <th>image</th>
-                  <th>link</th>
-                  <th>title</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                <tr>
-                  <td>Serguei</td>
-                  <td>Best book ever</td>
-                  <td>Image</td>
-                  <td>http://dd</td>
-                  <td>The sumer</td>
-                  <td>
-                    <Button color="success" size="sm" className="mr-2">Edit</Button>
-                    <Button color="danger" size="sm">Delete</Button>
-                  </td>
-                </tr>
-              </tbody>
-            
-            </Table>
-          
+      <Router>
+        <div>
+          <MyNavBar />
+         
         </div>
-      
+      </Router>
     )
   };
 };
